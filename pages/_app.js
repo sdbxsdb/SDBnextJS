736deb58455bb169b5284d940df7f5aefd50d742
Sam Donaghy-Bell <sam@reflex-studios.com>
2022-01-10 16:57:58 +0000

    dark light more working

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,20 @@
 import Layout from '../components/Layout'
 import '../styles/globals.css'
+import useDarkMode from "../useDarkMode";
 
 function MyApp({ Component, pageProps }) {
+  const [colorTheme, setTheme] = useDarkMode();
+
   return (
 
-      <Layout>
-        <div className="flex flex-1 max-w-[960px] mx-auto">
-          <Component {...pageProps} />
-        </div>
-      </Layout>
+      <div className="dark:bg-darkGray dark:text-lightGrey bg-lightGrey text-darkGrey">
+        <Layout>
+          <div className="flex flex-1 max-w-[960px] mx-auto ">
+          
+            <Component {...pageProps} />
+          </div>
+        </Layout>
+      </div>
 
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
+
 export default function Home() {
   return (
     <>
