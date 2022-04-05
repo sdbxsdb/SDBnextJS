ca663a800d5d61df71953c0686aa87c91bb5b2f5
sdbxsdb <69519326+sdbxsdb@users.noreply.github.com>
2022-04-05 17:02:26 +0100

    updated x and y scroll for modal

--- components/Modal.js
+++ components/Modal.js
@@ -17,11 +17,11 @@ export default function Modal({show, onClose, children}) {
   }
 
   const modalContent = show ? (
-    <div className="fixed top-0 left-0 w-full h-screen flex justify-center items-center dark:bg-darkGreyFaded dark:text-lightGrey bg-lightGreyFaded text-darkGrey" onClick={handleClose} >
+    <div className="fixed top-0 left-0 flex items-center justify-center w-full h-screen overflow-x-hidden overflow-y-auto dark:bg-darkGreyFaded dark:text-lightGrey bg-lightGreyFaded text-darkGrey">
       <div className='rounded-lg flex items-end flex-col z-50 overflow-scroll max-h-[500px] w-[500px] dark:bg-darkGrey dark:text-lightGrey bg-lightGrey text-darkGrey relative'>
  
             <button onClick={handleClose} className="h-[40px]  flex justify-end items-center fixed w-auto px-2 py-6 dark:bg-darkGrey dark:text-lightGrey bg-lightGrey text-darkGrey rounded-full">
-            <svg className="fill-current  dark:text-lightGrey  text-darkGrey hover:text-blue-400 transition"  width="30px" height="30px" x="0px" y="0px" viewBox="0 0 252 252" >
+            <svg className="transition fill-current dark:text-lightGrey text-darkGrey hover:text-blue-400"  width="30px" height="30px" x="0px" y="0px" viewBox="0 0 252 252" >
               <g>
                 <path d="M126,0C56.523,0,0,56.523,0,126s56.523,126,126,126s126-56.523,126-126S195.477,0,126,0z M126,234
                   c-59.551,0-108-48.449-108-108S66.449,18,126,18s108,48.449,108,108S185.551,234,126,234z"/>
