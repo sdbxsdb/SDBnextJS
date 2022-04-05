a4711d06ce7dc3c623018e880705d7c2cf0ddfec
sdbxsdb <69519326+sdbxsdb@users.noreply.github.com>
2022-04-05 16:53:05 +0100

    adding shoppy to site

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -7,55 +7,63 @@ const Projects = () => {
     <>
       <div>
       <div className="w-full text-center">
-        <h1 className="text-4xl font-semibold mb-12">Projects</h1>
+        <h1 className="mb-12 text-4xl font-semibold">Projects</h1>
       </div>
       <div className="flex justify-center">
-        <div className="w-1/2 flex flex-col items-start gap-y-12">
+        <div className="flex flex-col items-start w-1/2 gap-y-12">
 
-            <div className="border-b-2 dark:border-lightGrey border-darkGrey flex justify-start w-full transition group">
+            <div className="flex justify-start w-full transition border-b-2 dark:border-lightGrey border-darkGrey group">
               <Link href={'/projects/cinecopters'}>
-                <a className="flex items-center w-full justify-start group">
-                  <p className="group-hover:text-blue-400 transition transform group-hover:translate-x-4">CineCopters - (Broucher & Order Reqests)</p>
+                <a className="flex items-center justify-start w-full group">
+                  <p className="transition transform group-hover:text-blue-400 group-hover:translate-x-4">CineCopters - (Broucher & Order Reqests)</p>
                 </a>
               </Link>
             </div>
 
-            <div className="border-b-2 dark:border-lightGrey border-darkGrey flex justify-start w-full transition group">
+            <div className="flex justify-start w-full transition border-b-2 dark:border-lightGrey border-darkGrey group">
+              <Link href={'/projects/shoppy'}>
+                <a className="flex items-center justify-start w-full group">
+                  <p className="transition transform group-hover:text-blue-400 group-hover:translate-x-4">Faux Shop - (React and Typescript Demo)</p>
+                </a>
+              </Link>
+            </div>
+
+            <div className="flex justify-start w-full transition border-b-2 dark:border-lightGrey border-darkGrey group">
               <Link href={'/projects/kidsGame'}>
-                <a className="flex items-center w-full justify-start group">
-                  <p className="group-hover:text-blue-400 transition transform group-hover:translate-x-4">McDonalds Farm - (Interactive Childrens Game)</p>
+                <a className="flex items-center justify-start w-full group">
+                  <p className="transition transform group-hover:text-blue-400 group-hover:translate-x-4">McDonalds Farm - (Interactive Childrens Game)</p>
                 </a>
               </Link>
             </div>
             
-            <div className="border-b-2 dark:border-lightGrey border-darkGrey flex justify-start w-full transition group">
+            <div className="flex justify-start w-full transition border-b-2 dark:border-lightGrey border-darkGrey group">
               <Link href={'/projects/smartMirror'}>
-                <a className="flex items-center w-full justify-start group">
-                  <p className="group-hover:text-blue-400 transition transform group-hover:translate-x-4">Smart Mirror - (Raspberry Pi Build)</p>
+                <a className="flex items-center justify-start w-full group">
+                  <p className="transition transform group-hover:text-blue-400 group-hover:translate-x-4">Smart Mirror - (Raspberry Pi Build)</p>
                 </a>
               </Link>
             </div>
 
-            <div className="border-b-2 dark:border-lightGrey border-darkGrey flex justify-start w-full transition group">
+            <div className="flex justify-start w-full transition border-b-2 dark:border-lightGrey border-darkGrey group">
               <Link href={'/projects/fauxInstagram'}>
-                <a className="flex items-center w-full justify-start group">
-                  <p className="group-hover:text-blue-400 transition transform group-hover:translate-x-4">Faux Instagram - (Laravel Project)</p>
+                <a className="flex items-center justify-start w-full group">
+                  <p className="transition transform group-hover:text-blue-400 group-hover:translate-x-4">Faux Instagram - (Laravel Project)</p>
                 </a>
               </Link>
             </div>
 
-            <div className="border-b-2 dark:border-lightGrey border-darkGrey flex justify-start w-full transition group">
+            <div className="flex justify-start w-full transition border-b-2 dark:border-lightGrey border-darkGrey group">
               <Link href={'/projects/jnrClothing'}>
-                <a className="flex items-center w-full justify-start group">
-                  <p className="group-hover:text-blue-400 transition transform group-hover:translate-x-4">JnR Clothing - (Retail)</p>
+                <a className="flex items-center justify-start w-full group">
+                  <p className="transition transform group-hover:text-blue-400 group-hover:translate-x-4">JnR Clothing - (Retail)</p>
                 </a>
               </Link>
             </div>
 
-            <div className="border-b-2 dark:border-lightGrey border-darkGrey flex justify-start w-full transition group">
+            <div className="flex justify-start w-full transition border-b-2 dark:border-lightGrey border-darkGrey group">
               <Link href={'/projects/ruralVoices'}>
-                <a className="flex items-center w-full justify-start group">
-                  <p className="group-hover:text-blue-400 transition transform group-hover:translate-x-4">Rural Voices - (Public Service Information)</p>
+                <a className="flex items-center justify-start w-full group">
+                  <p className="transition transform group-hover:text-blue-400 group-hover:translate-x-4">Rural Voices - (Public Service Information)</p>
                 </a>
               </Link>
             </div>
---
+++ pages/projects/shoppy.js
@@ -0,0 +1,52 @@
+import MoreInfoCard from "../../components/MoreInfoCard";
+
+
+
+const shoppy = () => {
+  return ( 
+    <div className="absolute top-[112px] w-full left-0">
+
+      <div className="flex w-full justify-center items-center gap-x-12 h-[80px]">
+
+      <MoreInfoCard
+          content='
+          <div>
+            <div class="moreInfoContent">
+            <p>Being pretty new to React and very new to Typescript, I followed a tutorial and built this faux shop.  I used Material UI for a few components as well as my own custom ones using styled components. 
+            <br/><br/>
+            The site contains:
+            <br/><br/>
+            <ul class="listElements themeColorText">
+            <li>Fake Shop Products API</li>
+            <li>useState, useRef, useEffect</li>
+            <li>Page loader while fetching the data from the API</li>
+            <li>Total item quantity and price with dynamic changes</li>
+            <li>Total basked price with dynamic changes</li>
+
+            </ul><br>
+
+
+            
+            </p></div>
+            </div>
+            '
+        />
+
+        <a className="underline transition hover:text-blue-400" href="https://github.com/sdbxsdb/react-ts-shop" target="_blank" rel="noreferrer">GitHub</a>
+        <a className="underline transition hover:text-blue-400" href="https://react-ts-shop.vercel.app" target="_blank" rel="noreferrer">Live Site</a>
+      </div>
+
+      <div className="flex flex-col items-center mb-4">
+        <p className='text-smallerTitle'>Faux shop built with React &amp; Typescript</p>
+        <small>Please allow time for the external site below to load - it may take a few seconds.</small>
+      </div>
+
+      <div className="iframeWrapper dark:bg-darkGrey bg-lightGrey">
+        <iframe className="w-screen h-[calc(100vh-280px)]" src="https://react-ts-shop.vercel.app"></iframe>
+      </div>
+
+    </div>
+  );
+}
+ 
+export default shoppy;
